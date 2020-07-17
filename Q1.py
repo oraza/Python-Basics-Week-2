711b429ba571c82576c92e97168858ee2602ce2a
Owais Raza <reye27@gmail.com>
2020-07-17 23:12:12 +0500

    Q1
    Write one for loop to print out each character of the string my_str on a separate line.

---
+++ Q1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 23:10:12 2020
+
+@author: Owais Raza
+"""
+
+
+#Write one for loop to print out each character of the string my_str on a separate line.
+my_str = "MICHIGAN"
+for i in my_str:
+    print(i)
+
