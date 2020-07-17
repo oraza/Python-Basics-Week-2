8a46af68eb90a224a6e3c8367a3fd283758b0d07
Owais Raza <reye27@gmail.com>
2020-07-17 23:15:26 +0500

    Q2
    Write one for loop to print out each element of the list several_things. Then, write another for loop to print out the TYPE of each element of the list several_things. To complete this problem you should have written two different for loops, each of which iterates over the list several_things, but each of those 2 for loops should have a different result.

---
+++ Q2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 23:14:11 2020
+
+@author: Owais Raza
+"""
+
+
+several_things = ["hello", 2, 4, 6.0, 7.5, 234352354, "the end", "", 99]
+
+for i in several_things:
+    print(i)
+    
+for i in several_things:
+    print(type(i))
