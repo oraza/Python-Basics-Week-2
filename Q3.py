65a1b98f09cbec20b3f1aa7f20d6e69cb76004f2
Owais Raza <reye27@gmail.com>
2020-07-18 00:28:52 +0500

    Q3
    Write code that uses iteration to print out the length of each element of the list stored in str_list.

---
+++ Q3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 18 00:27:19 2020
+
+@author: Owais Raza
+"""
+#Write code that uses iteration to print out the length of each element of the list stored in str_list.
+
+
+str_list = ["hello", "", "goodbye", "wonderful", "I love Python"]
+
+# Write your code here.
+for i in str_list:
+    print(len(i))
