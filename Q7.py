7ac5f961b41caaf599370c3bfd10148e650b7359
Owais Raza <reye27@gmail.com>
2020-07-18 03:59:17 +0500

    Q7
    #Write code to create a list of numbers from 0 to 67 and assign that list 
    #to the variable nums. Do not hard code the list.

---
+++ Q7.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 18 03:57:08 2020
+
+@author: Owais Raza
+"""
+
+
+#Write code to create a list of numbers from 0 to 67 and assign that list 
+#to the variable nums. Do not hard code the list.
+
+nums = list(range(0,68))
+print(nums)
+
+
+
